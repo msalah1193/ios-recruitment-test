31fd858f98d78642e46fae1c38232ef30af18eda
Mohamed Salah <mohammed.salah1193@gmail.com>
2018-01-19 16:46:26 +0200

    edit AppDelegate class add model class "ItemModel" add empty view Model class

---
+++ iOSRecruitmentTest/ItemModel.swift
@@ -0,0 +1,35 @@
+//
+//  ItemModel.swift
+//  iOSRecruitmentTest
+//
+//  Created by Mohamed Salah on 1/19/18.
+//  Copyright © 2018 Snowdog. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import ObjectMapper
+
+class ItemModel: Object, Mappable{
+    
+    @objc dynamic var descriptionField: String!
+    @objc dynamic var icon: String!
+    @objc dynamic var id: Int = 0
+    @objc dynamic var name: String!
+    @objc dynamic var timestamp: Int = 0
+    @objc dynamic var url: String!
+    
+    required convenience init?(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map)
+    {
+        descriptionField <- map["description"]
+        icon <- map["icon"]
+        id <- map["id"]
+        name <- map["name"]
+        timestamp <- map["timestamp"]
+        url <- map["url"]
+    }
+}
---
+++ iOSRecruitmentTest/ViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  ViewModel.swift
+//  iOSRecruitmentTest
+//
+//  Created by Mohamed Salah on 1/19/18.
+//  Copyright © 2018 Snowdog. All rights reserved.
+//
+
+import Foundation
+
+class ViewModel {
+    //MARK: - Variables
+    
+    
+    //MARK:- Init
+    init() {
+        
+    }
+    
+    //MARK: - Filtring
+    
+    //MARK:- API CALLS
+    
+}
